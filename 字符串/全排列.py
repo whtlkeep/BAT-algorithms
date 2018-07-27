ebfce6bc8b7c3116dd7c35c36078ad3552e7a567
whtlkeep <wanghaoim@outlook.com>
2018-07-27 14:26:58 +0800

    添加了最长回文子串的动态规划实现

--- 字符串/全排列.py
+++ 字符串/全排列.py
@@ -2,15 +2,15 @@
 无重复 字符串的全排列  递归
 array=[1,2,3,4],start是排列的起始位
 """
-# def Permutation(array, start=0):
-#     size = len(array)
-#     if start == size-1:
-#         print(array)
-#         return
-#     for i in range(start,size):
-#         array[i],array[start] = array[start],array[i]
-#         Permutation(array,start+1)
-#         array[i], array[start] = array[start], array[i]
+def Permutation(array, start=0):
+    size = len(array)
+    if start == size-1:
+        print(array)
+        return
+    for i in range(start,size):
+        array[i],array[start] = array[start],array[i]
+        Permutation(array,start+1)
+        array[i], array[start] = array[start], array[i]
 
 
 # ---------------------- important --------------------------
@@ -33,9 +33,18 @@ def Permutation(array, start=0):
         array[i], array[start] = array[start], array[i]
 
 
-"""
-字符串的全排列 , 非递归
-array=[1,3,3,4],start是排列的起始位
+""" *******全排列的非递归算法*****************
+    * 思路：
+        起点：字典序最小的排列，例如 12345
+        终点：字典序最大的排序，例如 54321
+        过程：从当前排列生成字典序刚好比它大的下一个排列
+    * 如何求字典序中一个排序的下一个排列？
+        * 后找：字符串中最后一个升序的位置i, 即：
+                string[k]>string[k+1](k>i),string[i]<string[i+1].i之后都是降序
+        * 查找：string[i+1,...,n-1]中比string[i]大的最小值string[j]
+        * 交换：swap(i,j)
+        * 翻转：翻转string[i+1:n]
+
 """
 def GetNextPermutation(array):
     size = len(array)
---
+++ 字符串/最长回文子串.py
@@ -0,0 +1,49 @@
+"""
+    一、题目
+        求字符串中的最长回文子串。  形如 abba、aba都是
+    二、思路。 动态规划
+        dp[i][j]存的是string[i,...,j]是否是回文子串。是则为True, 不是则为False
+    三、步骤：
+        * dp初始化为False
+        * if j == i, 包含一个字符的串是回文串， dp[i][j] = True
+        * if j == i + 1,then:
+            * if string[i] == string[j], 长度为2的两个 相同 的字符构成的子串是回文串(例如，aa,bb,cc)，True
+            * if string[i] != string[j], 长度为2的两个 不同 的字符构成的子串不是回文串(例如，ac,ba,cf)，False
+        * if j > i + 1, 表示子串的长度至少为3，then: （思想，如果 *** 是回文串，则 a***a 也是回文串，a***b 不是回文串）
+            * if string[i] == string[j], 若dp[i+1][j-1]是回文，则dp[i][j]才是回文，否则不是
+            * if string[i] != string[j], then 不是回文
+"""
+
+def longestPalindrome(string):
+    size = len(string)
+    dp = []
+    # 初始化一个N * N的矩阵
+    for i in range(size):
+        row = []
+        for j in range(size):
+            row.append(False)
+        dp.append(row)
+    max_seq_len = 0
+    max_seq = ""
+    for i in range(size - 1, -1, -1):
+        for j in range(i, size):
+            if j == i:
+                dp[i][j] = True
+            elif j == i + 1:
+                if string[i] == string[j]:
+                    dp[i][j] = True
+                else:
+                    dp[i][j] = False
+            else:
+                if string[i] == string[j] and dp[i + 1][j - 1]:
+                    dp[i][j] = True
+                else:
+                    dp[i][j] = False
+            if j - i + 1 >= max_seq_len and dp[i][j]:
+                max_seq_len = j - i + 1
+                max_seq = string[i:j + 1]
+    return max_seq
+
+if __name__ == '__main__':
+    string = "yyyyabcdcbawe"
+    print(longestPalindrome(string))
