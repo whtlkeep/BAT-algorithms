84bd914b23908aa0c99d9cc9cfac6fc273c31eed
whtlkeep <wanghaoim@outlook.com>
2018-07-27 15:48:54 +0800

    a

--- 数组/排序.py
+++ 数组/排序.py
@@ -168,7 +168,6 @@ def quick_sort(alist, start, end):
     quick_sort(alist, start, low - 1)
     quick_sort(alist, low + 1, end)
 
-
 if __name__ == '__main__':
     alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
     quick_sort(alist, 0, len(alist)-1)
