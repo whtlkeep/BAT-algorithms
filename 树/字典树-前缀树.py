b3c817bbe54b29ea95637a8eee7082bc7191e1e3
whtlkeep <wanghaoim@outlook.com>
2018-08-02 22:53:59 +0800

    字典树

--- 树/字典树-前缀树.py
+++ 树/字典树-前缀树.py
@@ -1,5 +1,74 @@
 """
 一、题目
     实现一个字典树or前缀树，包含insert、search和startswith功能
+二、知识讲解
+    参考:
+        https://www.cnblogs.com/bonelee/p/8830825.html
 
 """
+
+
+
+class Node():
+    def __init__(self):
+        self.childs = [None] * 26
+        self.isLeaf = False
+
+class Trie(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = Node()
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: void
+        """
+        self.inserthelper(word, self.root)
+
+    def inserthelper(self, word, node):
+        if node == Node:
+            return
+        if len(word) == 0:
+            node.isLeaf = True
+            return
+        index = ord(word[0]) - ord('a')
+        if node.childs[index] == None:
+            node.childs[index] = Node()
+        self.inserthelper(word[1:], node.childs[index])
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        return self.searchhepler(word, self.root)
+
+    def searchhepler(self, word, node):
+        if node == None:
+            return False
+        if len(word) == 0:
+            return node.isLeaf
+        index = ord(word[0]) - ord('a')
+        return self.searchhepler(word[1:], node.childs[index])
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        return self.startsWithhelper(prefix, self.root)
+
+    def startsWithhelper(self, prefix, node):
+        if node == None:
+            return False
+        if len(prefix) == 0:
+            return True
+        index = ord(prefix[0]) - ord('a')
+        return self.startsWithhelper(prefix[1:], node.childs[index])
