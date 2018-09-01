ef65b129f90fcd48325fa6da9969f236bce80fae
whtlkeep <wanghaoim@outlook.com>
2018-09-01 20:24:42 +0800

    1

---
+++ The beauty of programming/chapter-3/3-计算字符串的相似度.py
@@ -0,0 +1,24 @@
+def calculate_string_distance(str_a, start_a, end_a, str_b, start_b, end_b):
+    if start_a > end_a:
+        if start_b > end_b:
+            return 0
+        else:
+            return end_b - start_b + 1
+    if start_b > end_b:
+        if start_a > end_a:
+            return 0
+        else:
+            return end_a - start_a + 1
+    if str_a[start_a] == str_b[start_b]:
+        return calculate_string_distance(str_a, start_a + 1, end_a, str_b, start_b + 1, end_b)
+    else:
+        t1 = calculate_string_distance(str_a, start_a + 1, end_a, str_b, start_b, end_b)
+        t2 = calculate_string_distance(str_a, start_a, end_a, str_b, start_b + 1, end_b)
+        t3 = calculate_string_distance(str_a, start_a + 1, end_a, str_b, start_b + 1, end_b)
+        return min(t1, t2, t3) + 1
+
+
+if __name__ == '__main__':
+    str_a = "aa"
+    str_b = "aac"
+    print(calculate_string_distance(str_a, 0, len(str_a) - 1, str_b, 0, len(str_b) - 1))
